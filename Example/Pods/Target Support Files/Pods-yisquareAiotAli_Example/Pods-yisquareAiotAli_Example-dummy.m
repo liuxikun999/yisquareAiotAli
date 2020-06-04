270221582a16e0170fe85163e80a0986435c200d
liuxikun <18774928141@139.com>
2020-06-04 11:06:59 +0800

    upload

---
+++ Example/Pods/Target Support Files/Pods-yisquareAiotAli_Example/Pods-yisquareAiotAli_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_yisquareAiotAli_Example : NSObject
+@end
+@implementation PodsDummy_Pods_yisquareAiotAli_Example
+@end
---
+++ Example/Pods/Target Support Files/yisquareAiotAli/yisquareAiotAli-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_yisquareAiotAli : NSObject
+@end
+@implementation PodsDummy_yisquareAiotAli
+@end
